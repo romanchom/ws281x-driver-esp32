9ff873db60c18b812af692efd083c8db572ec089
Roman Chomik <romanchom@gmail.com>
2018-12-23 21:07:37 +0100

    Removed std::vector from API

--- include/WS281xDriver.hpp
+++ include/WS281xDriver.hpp
@@ -9,7 +9,8 @@ class WS281xDriver
 public:
     explicit WS281xDriver(int pinNumber, size_t pixelCount);
     void send() const noexcept;
-    std::vector<uint8_t> & data() noexcept { return mData; }
+    uint8_t * data() noexcept { return mData.data(); }
+    size_t size() const noexcept { return mData.size(); }
 private:
     int mChannel;
     std::vector<uint8_t> mData;
